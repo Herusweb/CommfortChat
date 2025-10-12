f94f1a5dc2798a9e8b8646e6f7b05085cbc9fc56
Herusweb <herusweb@gmail.com>
2025-10-12 21:45:38 +0200

    feat: create chats page and main layout structure

---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex">
+        <div class="w-1/2 border border-grey-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+            ipsa rem ducimus minus mollitia illum quidem, explicabo fugiat sint
+            quos maiores at inventore facilis dolore fugit in ratione
+            exercitationem natus.
+        </div>
+        <div class="w-1/2 border border-grey-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam sed
+            cumque commodi officiis expedita dignissimos voluptas omnis neque
+            pariatur maiores perspiciatis, id atque. Eius, dolores perferendis
+            dolore nisi illum est.
+        </div>
+    </div>
+</template>
+
+<script>
+import Main from "@/Layouts/Main.vue";
+export default {
+    name: "Index",
+    layout: Main,
+};
+</script>
+
+<style lang="scss" scoped></style>
